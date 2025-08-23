835e1ac3a4bcaefb628c26353949769865d8166b
Dan <danttramirez137@gmail.com>
2025-08-23 01:26:15 -0300

    autoubiicacion y algunos marcadores con otro tipo de buscador

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// src/App.jsx
+import activities from "./data/activities.json";
 import MapView from "./components/MapView";
 
 export default function App() {
@@ -24,8 +24,8 @@ export default function App() {
 
       {/* Centro grande: MAPA (Contenedor 4) */}
       <div className="div4">
-        <div className="container4">
-          <MapView />
+        <div className="container4" style={{ height: "70vh" }}>
+          <MapView items={activities} />
         </div>
       </div>
 
@@ -34,9 +34,9 @@ export default function App() {
         <div className="card">Contenedor 6</div>
       </div>
     </div>
-    // 
+    //
     // Asi tendrian que estar las rutas
-    // 
+    //
     // <Routes>
     //                     <Route path="/" element={<AuthLayout />}>
     //                         <Route index element={<Login />} />
@@ -48,4 +48,3 @@ export default function App() {
     //                 </Routes>
   );
 }
-
