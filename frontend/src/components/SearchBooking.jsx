58e6444047a9387bd00a1214d8f97df09568d706
MaxiLeguiza <leguimanfire17@gmail.com>
2025-09-02 21:42:53 -0300

    Se agrega Menu - Frontend sin funcionalidad

--- frontend/src/components/SearchBooking.jsx
+++ frontend/src/components/SearchBooking.jsx
@@ -7,18 +7,18 @@ const SearchBooking = () => {
 
     const handleSearch = (e) => {
         e.preventDefault();
-        console.log("Buscando disponibinilidad ", { checkIn, checkOut, personas });
+        console.log("Buscando disponibinilidad... ", { checkIn, checkOut, personas });
         // Aquí iría la lógica para buscar disponibilidad
     };
     return (
         <div className="max-w-lg mx-auto p-6 bg-white rounded-2xl shadow-lg">
-            <h2 className="text-2xl font-bold mb-4 text-center">
-                Buscar Disponibilidad
+            <h2 className="text-2xl font-bold mb-4 text-center text-black">
+                Encuentra tu aventura
             </h2>
             <form onSubmit={handleSearch} className="space-y-4">
                 {/* Check-in */}
                 <div>
-                    <label className="block text-gray-700 mb-1">Check-In</label>
+                    <label className="block text-gray-700 mb-1">Ingreso</label>
                     <input
                         type="date"
                         value={checkIn}
@@ -30,7 +30,7 @@ const SearchBooking = () => {
 
                 {/* Check-out */}
                 <div>
-                    <label className="block text-gray-700 mb-1">Check-Out</label>
+                    <label className="block text-gray-700 mb-1">Salida</label>
                     <input
                         type="date"
                         value={checkOut}
@@ -55,7 +55,8 @@ const SearchBooking = () => {
                 {/* Botón */}
                 <button
                     type="submit"
-                    className="w-full bg-blue-600 text-white font-semibold py-2 px-4 rounded-xl hover:bg-blue-700 transition"
+                    className="w-full bg-orange-600 text-white font-semibold py-2 px-4 rounded-xl hover:bg-blue-700 transition"
+                    
                 >
                     Buscar
                 </button>
