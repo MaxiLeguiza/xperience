d9e26854ae72ad5b89f1d088a5f17f6b273f50e5
MaxiLeguiza <leguimanfire17@gmail.com>
2025-09-08 00:41:49 -0300

    Proceso de reserva - No guarda datos sobre Base de datos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,25 +6,36 @@ import Login from "./pages/Login";
 import Registrar from "./pages/Registrar";
 import SearchBooking from "./components/SearchBooking";
 import SearchResults from "./components/Booking/SearchResults";
+import Cart from "./components/Booking/Cart";          
+import Confirmacion from "./components/Booking/Confirmacion";
+import Exito from "./components/Booking/Exito";
 
 import { Routes, Route } from "react-router-dom";
 
 
 export default function App() {
   return (
-    
+
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="registrar" element={<Registrar />} />
       {/* Página principal con el buscador */}
-        <Route path="/" element={<SearchBooking />} />
-        {/* Página de resultados */}
+      <Route path="/" element={<SearchBooking />} />
+      {/* Página de resultados */}
       <Route path="/resultados" element={<SearchResults />} />
       {/* <Route path="olvide-password" element={<OlvidePassword />} />
       <Route path="olvide-password/:token" element={<NuevoPassword />} />
       <Route path="confirmar/:id" element={<ConfirmarCuenta />} /> */}
+      {/* Carrito de reservas */}
+      <Route path="/carrito" element={<Cart />} />
+
+      {/* Confirmación de reserva */}
+      <Route path="/confirmacion" element={<Confirmacion />} />
+      {/* Página de éxito */}
+      <Route path="/exito" element={<Exito />} /> {/* Nueva ruta */}
+
     </Routes>
-    
+
   );
 }
