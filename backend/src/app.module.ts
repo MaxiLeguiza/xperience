21a7248a97326b420bdcaa523cd304aa85202e1f
maxidaniele <maxidanielemg@gmail.com>
2025-09-22 01:21:50 -0300

    agregando recorrido al backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { ReservaModule } from './reserva/reserva.module';
 import { NotificationsModule } from './notifications/notifications.module';
+import { RecorridoModule } from './recorrido/recorrido.module';
+
 
 @Module({
   imports: [
@@ -13,6 +15,7 @@ import { NotificationsModule } from './notifications/notifications.module';
     CommonModule,
     ReservaModule,
     NotificationsModule,
+    RecorridoModule,
   ],
 })
 export class AppModule {}
