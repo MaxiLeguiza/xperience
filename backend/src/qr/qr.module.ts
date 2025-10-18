78111f72c16cbfcea39f7019e3a86f079188e32f
MaxiLeguiza <leguimanfire17@gmail.com>
2025-10-18 10:29:27 -0300

    Commit para pull

--- backend/src/qr/qr.module.ts
+++ backend/src/qr/qr.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+  import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { QrController } from './qr.controller';
 import { QrService } from './qr.service';
